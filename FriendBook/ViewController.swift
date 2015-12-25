42a597697ddb3714b6b41aee6a84e8961716c15e
aparna krishnan <aparnalocked@gmail.com>
2015-12-25 05:12:17 +0400

    Initial Commit

---
+++ FriendBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendBook
+//
+//  Created by Aparna Krishnan on 12/25/15.
+//  Copyright © 2015 Aparna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
